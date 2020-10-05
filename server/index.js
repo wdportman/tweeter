cbb82fa9f53d81fcdb1f1802f86340dcbed1e195
Will <wdportman@gmail.com>
2020-10-05 21:55:48 +0000

    Fork and clone starter code

--- server/index.js
+++ server/index.js
@@ -30,5 +30,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log("Tweeter listening on port " + PORT);
 });
