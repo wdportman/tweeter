f6c67ffa20ed9ac74235d3c6440e5f24bec20976
Will <wdportman@gmail.com>
2020-10-08 00:16:31 +0000

    Create dynamic tweet rendering in client.js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1475885334000
+  }
+]
+
+const createTweetElement = function(tweet) {
+  const tweetHTML =
+    `<article class="tweet">
+        <header>
+          <div class="picAndName">
+            <img src=${tweet.user.avatars}>
+            <span class="name">${tweet.user.name}</span>
+          </div>
+          <span class="username">${tweet.user.handle}</span>
+        </header>
+        <p class="tweetBody">${tweet.content.text}</p>
+        <footer>
+          <div class="content">
+            <span class="timestamp">${moment(tweet.created_at).fromNow()}</span>
+            <div class="icons">
+              <i class="fas fa-flag" class="icon"></i>
+              <i class="fas fa-retweet"class="icon"></i>
+              <i class="fas fa-heart"class="icon"></i>
+            </div>
+          </div>
+        </footer>
+      </article>`;
+  return tweetHTML;
+};
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
+
+//Driver code:
+
+$(() => {
+  renderTweets(tweetData);
+})
